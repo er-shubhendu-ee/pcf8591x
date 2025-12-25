902820fcef571d8a9c560304cc444ae2457ec079
shubhendu <er.shubhendu.ee@gmail.com>
2025-12-25 15:37:55 +0530

    initial routines written

--- pcf8591x/pcf8591x.c
+++ pcf8591x/pcf8591x.c
@@ -10,3 +10,63 @@
  **/
 
 #include "pcf8591x.h"
+
+#define PCF8591X_MAX_COUNT 8
+
+/*!
+ * @def         PCF8591X_AT_INDEX
+ * @brief       //  Currently supports single chip on an I2C bus. Multi-instance support to be added
+ *              later on
+ * @details
+ *
+ **/
+#define PCF8591X_MULTIINDEX_SUPPORTED 0
+
+#ifndef LOG_LEVEL
+#define LOG_LEVEL 3
+#endif
+
+#define TAG "PCF8591X"
+
+typedef struct pcf8591x_tagInstanceStruct {
+    union {
+        uint8_t addrByte;  //
+        struct {
+            uint8_t a0State : 1;
+            uint8_t a1State : 1;
+            uint8_t a2State : 1;
+            uint8_t fixedBits : 5
+        } addr;
+    };
+    pcf8591x_I2CWriteBytes_t I2CWriteFn;
+    pcf8591x_I2CWriteBytes_t I2CReadFn;
+} pcf8591x_InstanceStruct_t;
+
+static pcf8591x_InstanceStruct_t gInstanceArr[PCF8591X_MAX_COUNT];
+
+int pcf8591x_init(uint8_t deviceId, pcf8591x_Config_t *pConfig) {
+    if (!pConfig) {                             //!< NULL pointer
+        return PCF8591X_STATUS_ERROR_NULL_ARG;  //!< Return error
+    }
+
+    /*!< Multi-index not supported */
+    if (0 == PCF8591X_MULTIINDEX_SUPPORTED) {
+        if (!deviceId) {
+            return PCF8591X_STATUS_ERROR_BAD_ARG;
+        }
+
+        gInstanceArr[0].addr.a0State = pConfig->addrSelect.a0State;
+        gInstanceArr[0].addr.a1State = pConfig->addrSelect.a1State;
+        gInstanceArr[0].addr.a2State = pConfig->addrSelect.a2State;
+        gInstanceArr[0].I2CWriteFn = pConfig->portI2CWriteFn;
+        gInstanceArr[0].I2CReadFn = pConfig->portI2CReadFn;
+    }
+
+    gInstanceArr[0].addr.a0State = pConfig->addrSelect.a0State;
+    gInstanceArr[0].addr.a1State = pConfig->addrSelect.a1State;
+    gInstanceArr[0].addr.a2State = pConfig->addrSelect.a2State;
+    gInstanceArr[0].I2CWriteFn = pConfig->portI2CWriteFn;
+    gInstanceArr[0].I2CReadFn = pConfig->portI2CReadFn;
+
+    return PCF8591X_STATUS_OK;  //!< Return no error
+}
--- pcf8591x/pcf8591x.h
+++ pcf8591x/pcf8591x.h
@@ -14,11 +14,35 @@
 
 #include <stdint.h>
 
+typedef enum pcf8591x_tagStatus {
+    PCF8591X_STATUS_OK = 0,
+    PCF8591X_STATUS_BUSY = 1,
+
+    PCF8591X_STATUS_ERROR = -1,
+    PCF8591X_STATUS_ERROR_NULL_ARG = -2,
+    PCF8591X_STATUS_ERROR_BAD_ARG = -3,
+} pcf8591x_Status_t;
+
+typedef int (*pcf8591x_I2CWriteBytes_t)(void *pCtx, uint8_t slaveAddr, uint8_t *pDataBuff,
+                                        uint16_t buffSize);
+typedef int (*pcf8591x_I2CReadBytes_t)(void *pCtx, uint8_t slaveAddr, uint8_t *pDataBuff,
+                                       uint16_t buffSize);
+
+typedef struct pcf8591x_tagConfig {
+    struct {
+        uint8_t a0State : 1;
+        uint8_t a1State : 1;
+        uint8_t a2State : 1;
+    } addrSelect;
+    pcf8591x_I2CWriteBytes_t portI2CWriteFn;
+    pcf8591x_I2CWriteBytes_t portI2CReadFn;
+} pcf8591x_Config_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-int pcf8591x_init(void);
+int pcf8591x_init(uint8_t deviceId, pcf8591x_Config_t *pCommConfig);
 
 #ifdef __cplusplus
 }
