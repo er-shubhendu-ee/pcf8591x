1fdd1c5c1c4c7dccdc5d6105fd7c163b5830d4cd
shubhendu <er.shubhendu.ee@gmail.com>
2025-12-25 14:18:15 +0530

    initial commit

---
+++ pcf8591x/pcf8591x.c
@@ -0,0 +1,12 @@
+/**
+ * @file      pcf8591x.c
+ * @author:   Shubhendu B B
+ * @date:     25/12/2025
+ * @brief
+ * @details
+ *
+ * @copyright
+ *
+ **/
+
+#include "pcf8591x.h"
---
+++ pcf8591x/pcf8591x.h
@@ -0,0 +1,27 @@
+/**
+ * @file      pcf8591x.h
+ * @author:   Shubhendu B B
+ * @date:     25/12/2025
+ * @brief
+ * @details
+ *
+ * @copyright
+ *
+ **/
+
+#ifndef __PCF8591X_H__
+#define __PCF8591X_H__
+
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int pcf8591x_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* @end  _PCF8591X_H_*/
